bde04b777852775761d7678e71062e4b78d45365
Daniyil Vasylenko <danielspr3@gmail.com>
2018-06-17 23:37:23 +0300

    Remove postMessage

--- src/routes/gh-auth-callback.js
+++ src/routes/gh-auth-callback.js
@@ -1,6 +1,5 @@
 const uuid = require('uuid');
 
 module.exports = (req, res) => {
-  const data = { ...req.user._json, token: req.user.accessToken, appId: uuid.v4() };
-  res.status(200).send(`<script>parent.window.opener.postMessage(${JSON.stringify(data)}, 'http://localhost')</script>`);
+  res.status(200).send({ ...req.user._json, token: req.user.accessToken, appId: uuid.v4() });
 }
